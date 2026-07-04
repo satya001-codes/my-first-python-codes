7dc693467052b7022597dc691c765d4179c35cf4
satya001-codes <bhartisatya001@gmail.com>
2026-07-04 10:15:15 +0530

    Add files via upload

---
+++ average.py
@@ -0,0 +1,6 @@
+#program to find the average of 3 numbers
+num1 = float(input("enter the 1st number:"))
+num2 = float(input("enter the 2nd number:"))
+num3 = float(input("enter the 3rd number:"))
+avg = (num1+num2+num3)/3
+print("the average of above 3 numbers are:",avg)
---
+++ repeat.py
@@ -0,0 +1,4 @@
+str = "Good Morning!"
+print(str)
+n = int(input("no of times the string to be repeated:"))
+print(str*n)
