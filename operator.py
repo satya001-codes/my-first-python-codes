569ea48c678cfa7b33889173691f55910d29ea5a
satya001-codes <bhartisatya001@gmail.com>
2026-07-08 11:31:29 +0530

    Add files via upload

---
+++ operator.py
@@ -0,0 +1,14 @@
+a=float(input("enter the 1st number:"))
+b=float(input("enter the 2nd number:"))
+print("the sum of the above two numbers is",a+b)
+if a>=b:
+        print("the difference of the above two numbers is",a-b)
+else:
+        print("the difference of the above two numbers is",b-a)
+print("the product of the above two numbers is",a*b)
+if a>=b:
+        print("the remainder of the above two numbers is",a%b)
+else:
+        print("the remainder of the above two numbers is",b%a)
+
+
