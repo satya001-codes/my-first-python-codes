3fca1a2849bc3593fbc76e222546277b09b22686
satya001-codes <bhartisatya001@gmail.com>
2026-07-20 10:28:21 +0530

    Add files via upload

---
+++ swapin.py
@@ -0,0 +1,9 @@
+#using the inbuilt swapcase function of python to swap cases
+def swapcase(s):
+    return s.swapcase()
+
+
+
+s=input("enter the tet that has to be swapped:")
+result=swapcase(s)
+print (result)
